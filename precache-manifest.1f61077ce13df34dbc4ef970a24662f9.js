13379ee32d169bd440b107f68bd2e88c8f1cf909
Abebe-Adamu <abbkassa@gmail.com>
2020-09-05 23:51:29 -0400

    Updates

---
+++ precache-manifest.1f61077ce13df34dbc4ef970a24662f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c7b5cc8a9456b46f1edffa4de7b8a7",
+    "url": "/my-portfolio-app/index.html"
+  },
+  {
+    "revision": "9e328f8f9378c68e7e5a",
+    "url": "/my-portfolio-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f4d64557f7246b131ae7",
+    "url": "/my-portfolio-app/static/js/2.6385edd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio-app/static/js/2.6385edd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e328f8f9378c68e7e5a",
+    "url": "/my-portfolio-app/static/js/main.b3ff5c32.chunk.js"
+  },
+  {
+    "revision": "2e01c44ac1564f490570",
+    "url": "/my-portfolio-app/static/js/runtime-main.b0f29e76.js"
+  }
+]);
